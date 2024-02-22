ac66f5621caf7f2faec63da665e3dd16fffacb71
tonnytech <tonnytei4@gmail.com>
2024-02-22 07:50:03 +0300

    Add complete service title

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,16 +5,18 @@ import serviceTwo from '../Assets/homepage/mt-1979-home-img02.png'
 
 const Services = () => {
     return (
-        <section className='service_image_wrapper'>
-            <div className='service_image_one_wrapper'>
-                <img src={serviceOne} alt='service_image_one' className='service_image_one' />
-            </div>
-            <div className='service_image_two_wrapper'>
-                <img src={serviceTwo} alt='service_image_two' className='service_image_two' />
-            </div>
-            <div className='services_header_wrapper'>
-                <h1>What The Office offers you</h1>
-                <h2>OUR SERVICES</h2>
+        <section className='service_section'>
+            <div className='service_image_wrapper'>
+                <div className='service_image_one_wrapper'>
+                    <img src={serviceOne} alt='service_image_one' className='service_image_one' />
+                </div>
+                <div className='service_image_two_wrapper'>
+                    <img src={serviceTwo} alt='service_image_two' className='service_image_two' />
+                </div>
+                <div className='services_header_wrapper'>
+                    <h1>What The Office offers you</h1>
+                    <h2>OUR SERVICES</h2>
+                </div>
             </div>
         </section>
     );
